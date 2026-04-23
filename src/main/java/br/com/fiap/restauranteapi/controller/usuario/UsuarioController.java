becb40f5dd057a4f0c509bc8d164d2452d55a99a
RobertoLocatelli02 <rlocatelli02@gmail.com>
2026-04-23 20:52:29 -0300

    RES-7

--- src/main/java/br/com/fiap/restauranteapi/controller/usuario/UsuarioController.java
+++ src/main/java/br/com/fiap/restauranteapi/controller/usuario/UsuarioController.java
@@ -1,11 +1,15 @@
 package br.com.fiap.restauranteapi.controller.usuario;
 
 import br.com.fiap.restauranteapi.model.dto.usuario.UsuarioDTO;
+import br.com.fiap.restauranteapi.model.entity.usuario.Usuario;
+import br.com.fiap.restauranteapi.model.response.MensagemSucessoResponse;
 import br.com.fiap.restauranteapi.service.usuario.UsuarioService;
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,4 +30,11 @@ public class UsuarioController implements UsuarioDocs {
     public ResponseEntity<UsuarioDTO> getUsuarioByNome(@RequestParam @NotBlank String nome) {
         return ResponseEntity.ok(usuarioService.getUsuarioByNome(nome));
     }
+
+    @PostMapping
+    public ResponseEntity<MensagemSucessoResponse> createUser(@RequestBody @Valid Usuario usuario) {
+        var response = usuarioService.createUser(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
--- src/main/java/br/com/fiap/restauranteapi/service/usuario/UsuarioService.java
+++ src/main/java/br/com/fiap/restauranteapi/service/usuario/UsuarioService.java
@@ -3,8 +3,11 @@ package br.com.fiap.restauranteapi.service.usuario;
 import br.com.fiap.restauranteapi.exceptions.UsuarioNotFoundException;
 import br.com.fiap.restauranteapi.model.dto.usuario.UsuarioDTO;
 import br.com.fiap.restauranteapi.model.entity.usuario.Usuario;
+import br.com.fiap.restauranteapi.model.response.MensagemSucessoResponse;
 import br.com.fiap.restauranteapi.repository.usuario.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Usuario getUsuarioByLogin(String pLogin) {
         return usuarioRepository.findByLogin(pLogin).orElseThrow(() -> new UsuarioNotFoundException("O Usuário com o login informado não foi encontrado!"));
@@ -42,5 +46,13 @@ public class UsuarioService {
                 usuario.getTipoUsuario().getDescricao(),
                 usuario.getSituacaoCadastro().getDescricao(),
                 usuario.getDataAlteracao());
+    }
+
+    @Transactional
+    public MensagemSucessoResponse createUser(Usuario usuario) {
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        usuarioRepository.save(usuario);
+
+        return new MensagemSucessoResponse(HttpStatus.CREATED.value(), "Usuário cadastrado com sucesso!");
     }
 }
